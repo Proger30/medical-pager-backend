9eb0ebc0ad7d6008459fe31b7991bc3827a6d0df
f8b4i2 <45534457+Proger30@users.noreply.github.com>
2023-05-14 22:17:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World!!');
 });
 
 app.post('/', (req, res) => {
